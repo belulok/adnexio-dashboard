1530dcea637b51399ffad9988fcb05da00d8f2e0
belulok <belulok_sebastian@hot,mail.com>
2024-09-15 15:28:41 +0800

    addded shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { fontFamily } from "tailwindcss/defaultTheme";
 
+module.exports = {
+  //...
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
